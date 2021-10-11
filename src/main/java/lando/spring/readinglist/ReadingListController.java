4c268597247c383e6c568a05ff8351963e785a0d
Lando Icaza <landoicco@gmail.com>
2021-10-11 09:03:33 -0500

    Custom error page added!!

--- src/main/java/lando/spring/readinglist/ReadingListController.java
+++ src/main/java/lando/spring/readinglist/ReadingListController.java
@@ -1,7 +1,6 @@
 package lando.spring.readinglist;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
